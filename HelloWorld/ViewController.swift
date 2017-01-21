1f7e0fba34107e0a83ffa9de294bbb2c73413d30
Country.Girls <country.girls@109-b-2tugfwm-m.manorisd.net>
2017-01-21 14:41:01 -0600

    first commit

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var banner: UIImageView!
+    @IBOutlet weak var welcomeBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func welcomePressed(_ sender: Any) {
+        background.isHidden = false
+        banner.isHidden = false
+        welcomeBtn.isHidden = true
+    }
 
 }
 
